1bcc385c7cfa21c4ebbce38961b933c77ac8abe4
acatalina <accarrasco@gmail.com>
2017-03-22 20:22:27 +0000

    delay: add test to check function hasn not been called before the timeout

--- spec/delay.spec.js
+++ spec/delay.spec.js
@@ -2,30 +2,31 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var delay = require('../advancedLowbar').delay;
 
-describe('delay', function () {
-  it('is a function', function () {
+describe('delay', function() {
+  it('is a function', function() {
     expect(delay).to.be.a('function');
   });
   
   it('calls a function after a specified amount of time', function(done) {
     let spy = sinon.spy();
-    delay(spy, 500);
-    
+    delay(spy, 50);
+    expect(spy.calledOnce).to.be.false;
+
     setTimeout(function() {
-      expect(spy.calledOnce).to.equal(true);
+      expect(spy.calledOnce).to.be.true;
       done();
-    }, 800);
+    }, 100);
   });
 
-  it('it passes arguments if given to the function', function(done) {
+  it('passes the arguments given to the function', function(done) {
     let spy = sinon.spy();
-    delay(spy, 500, 5, 6);
+    delay(spy, 50, 5, 6);
     
     setTimeout(function() {
-      expect(spy.calledOnce).to.equal(true);
+      expect(spy.calledOnce).to.be.true;
       expect(spy.calledWithExactly(5, 6)).to.equal(true);
       done();
-    }, 800);
+    }, 100);
   });
 });
 
