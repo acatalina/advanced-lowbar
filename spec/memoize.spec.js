655b1768c058526eb4e54989768d8e2b1524f412
acatalina <accarrasco@gmail.com>
2017-03-22 20:07:27 +0000

    memoize: add extra tests for cache and hashFunction

--- spec/memoize.spec.js
+++ spec/memoize.spec.js
@@ -8,23 +8,43 @@ describe('memoize', function () {
   });
   
   it('it does what the input function does', function () {
-    let test = function(n) { return n * 2; };
-    let tester = memoize(test);
+    let iteratee = function(n) { return n * 2; };
+    let tester = memoize(iteratee);
+    
     let actual = tester(2);
-    let actual2 = tester(4);
     let expected = 4;
-    let expected2 = 8;
     expect(actual).to.equal(expected);
-    expect(actual2).to.equal(expected2);
   });
 
-  it('it does what the input function does', function () {
-    let spy = sinon.spy(function (x) { return x; });
+  it('holds the values computed not having to compute them again', function() {
+    let spy = sinon.spy(function(n) { return n * 2; });
     let tester = memoize(spy);
     tester(2);
     tester(2);
+    
     let actual = spy.callCount;
     let expected = 1;
     expect(actual).to.equal(expected);
+  });
+
+  it('has a cache method that returns the cache stored', function() {
+    let iteratee = function(n) { return n * 2; };
+    let tester = memoize(iteratee);
+    tester(2);
+    
+    let actual = tester.cache;
+    let expected = {2: 4};
+    expect(actual).to.eql(expected);
+  });
+
+  it('allows a hashFunction as a second argument to compute the hash key for storing', function() {
+    let iteratee = function(n) { return n * 2; };
+    let hashFunction = function(n) { return 'num' + n; };
+    let tester = memoize(iteratee, hashFunction);
+    tester(2);
+    
+    let actual = tester.cache;
+    let expected = {'num2': 4};
+    expect(actual).to.eql(expected);
   });
 });
