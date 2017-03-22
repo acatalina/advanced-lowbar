4350f16bce0e184ef0a3c80165894899536e3d9e
acatalina <accarrasco@gmail.com>
2017-03-22 19:26:12 +0000

    indexOf: refactor

--- advancedLowbar.js
+++ advancedLowbar.js
@@ -1,13 +1,15 @@
 var _ = {};
 
 _.indexOf = function(arr, val, isSorted) {
-  if (isSorted === true) {
-    var startIndex = 0;
-    var prevIndex = 0;
-    var endIndex = arr.length;
-    var midIndex = Math.floor(endIndex / 2);
+  if (!Array.isArray(arr) || !val) return -1;
 
-    while ( (arr[midIndex] !== val) && (prevIndex / midIndex !== 1) ) {
+  if (isSorted === true) {
+    let startIndex = 0;
+    let prevIndex = 0;
+    let endIndex = arr.length;
+    let midIndex = Math.floor(endIndex / 2);
+
+    while ((arr[midIndex] !== val) && (prevIndex / midIndex !== 1)) {
       if (arr[midIndex] > val) {
         endIndex = midIndex;
         prevIndex = midIndex;
@@ -49,9 +51,30 @@ _.once = function(fun) {
   };
 };
 
-_.memoize = function() {
+_.memoize = function(fun) {
+  var memo = {};
 
+  const speedy = function() {
+    const args = JSON.stringify(arguments[0]);
+    
+    if (memo[args]) {
+      return memo[args];
+    }
+    memo[args] = fun.apply(null, arguments);
+    return memo[args];
+  };
+
+  speedy.cache = memo;
+  
+  return speedy;
 };
 
+_.delay = function(fun, wait) {
+  const args = Array.from(arguments).slice(2);
+  
+  setTimeout(function() {
+    fun.apply(null, args);
+  }, wait);
+}; 
 
 module.exports = _;
